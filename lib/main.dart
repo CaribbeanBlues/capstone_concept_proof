7d03e401e38de0ec8dd7e1081f8b489ab43c99b2
PrinceCarryan <jkjohnson246@gmail.com>
2022-03-03 19:40:54 -0400

    Added: - 3 Dart files*   - *Stolen and mended together from TFLite Flutter Helper Github - 3 label and 3 model files - Many dependencies. Namely tflite helper, tflite - install.bat as a tflite dependency for Android   - iOS version to be added and tested*
    Removed:
    - Commented out the previous main.dart contents
    - No longer using image_model.dart nor homepage.dart atm
      - Designs to be borrowed/edited
    
    To Do:
    - Design
    - Cleaning and refactoring

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,146 @@
+// import 'package:flutter/material.dart';
+//
+// import 'homepage.dart';
+//
+// void main() {
+//   runApp(const MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Proof of Concept',
+//       theme: ThemeData(
+//         primarySwatch: Colors.blue,
+//       ),
+//       home: const MyHomePage(title: 'Image Labelling'),
+//     );
+//   }
+// }
+//
+
+import 'dart:io';
+import 'package:image/image.dart' as img;
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'classifier.dart';
+import 'classifier_quant.dart';
+import 'package:logger/logger.dart';
+import 'package:tflite_flutter_helper/tflite_flutter_helper.dart';
+import 'image_model.dart';
 
-import 'homepage.dart';
-
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Proof of Concept',
+      title: 'Image Classification',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Image Labelling'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, this.title}) : super(key: key);
+
+  final String? title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late Classifier _classifier;
+
+  var logger = Logger();
+
+  File? _image;
+  final picker = ImagePicker();
+
+  Image? _imageWidget;
+
+  img.Image? fox;
+
+  Category? category;
+
+  ImageModel imageModel = ImageModel();
+  @override
+  void initState() {
+    super.initState();
+    _classifier = ClassifierQuant();
+  }
+
+  Future getImage() async {
+    final pickedFile = await picker.pickImage(source: ImageSource.camera);
+
+    setState(() {
+      _image = File(pickedFile!.path);
+      _imageWidget = Image.file(_image!);
+
+      _predict();
+    });
+  }
+
+  void _predict() async {
+    img.Image imageInput = img.decodeImage(_image!.readAsBytesSync())!;
+    var pred = _classifier.predict(imageInput);
+
+    setState(() {
+      this.category = pred;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('TfLite Flutter Helper',
+            style: TextStyle(color: Colors.white)),
+      ),
+      body: Column(
+        children: <Widget>[
+          Center(
+            child: _image == null
+                ? Text('No image selected.')
+                : Container(
+                    constraints: BoxConstraints(
+                        maxHeight: MediaQuery.of(context).size.height / 2),
+                    decoration: BoxDecoration(
+                      border: Border.all(),
+                    ),
+                    child: _imageWidget,
+                  ),
+          ),
+          SizedBox(
+            height: 36,
+          ),
+          Text(
+            category != null ? category!.label : '',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+          ),
+          SizedBox(
+            height: 8,
+          ),
+          Text(
+            category != null
+                ? 'Confidence: ${category!.score.toStringAsFixed(3)}'
+                : '',
+            style: TextStyle(fontSize: 16),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: getImage,
+        tooltip: 'Pick Image',
+        child: Icon(Icons.add_a_photo),
+      ),
     );
   }
 }
